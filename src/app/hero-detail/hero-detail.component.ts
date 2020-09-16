f065512a2179746e4863973bd56ef2a368f91b8e
Tomasz Krzysztof Sobota <tkso@netcompany.com>
2020-09-16 23:17:13 +0200

    Added routing to project.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero} from "../Hero";
+import {Component, Input, OnInit} from '@angular/core';
+import {Hero} from "../Hero";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,7 +10,12 @@ import { Hero} from "../Hero";
 export class HeroDetailComponent implements OnInit {
   @Input() hero : Hero;
 
-  constructor() { }
+  constructor(private messageService : MessageService) { }
+
+  nameChange(hero : Hero) : void {
+    console.log(hero.name);
+    this.messageService.add("HeroDetailComponent: Changed hero name to: " + hero.name);
+  }
 
   ngOnInit(): void {
   }
