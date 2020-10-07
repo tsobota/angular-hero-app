d269b953024351a4eebb0768f19e36a1a13f6cd4
Tomasz Krzysztof Sobota <tkso@netcompany.com>
2020-10-07 17:51:17 +0200

    Added in-memory testing service with data.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,21 +3,29 @@ import {Observable, of} from "rxjs";
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import {MessageService} from "./message.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService : MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService : MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id : number) : Observable<Hero> {
     this.messageService.add('HeroService: fetched hero id= '+id);
     return of(HEROES.find(hero => hero.id === id));
+  }
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
